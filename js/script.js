b90715202ab3a07b23a4995fc7f02c04805249b2
CzupakCzupak <128129770+CzupakCzupak@users.noreply.github.com>
2023-05-22 15:06:56 +0200

    comment

--- js/script.js
+++ js/script.js
@@ -25,4 +25,105 @@ splidesAmazed.forEach(item => {
 
 })
 
+const select = document.querySelector('.car-select-btn')
+const selectOptions = document.querySelectorAll('.car-option-box')
+const body = document.body
+const ctaHidden = document.querySelector('.cta-hidden')
+
+const offerSelect = document.querySelector('.offer-select')
+
+
+body.addEventListener('click', closeList)
+select.addEventListener('click', listActive)
+offerSelect.addEventListener('click', listActive)
+// selectOptions.forEach(item =>{
+//     item.addEventListener('click',changeAttr)
+// })
+
+function closeList(e){
+    if(!e.target.classList.contains('car-select-btn') && !e.target.closest('.car-select-btn')){
+        select.classList.remove('car-select-active')
+        selectOptions.forEach(item => {
+            item.classList.remove('car-option-active')
+        })
+        flag = 0
+    }
+}
+
+let flag = 0
+let optionsChildren
+function listActive(e){
+    if(!e.target.closest('.offer-select')){
+        select.classList.add('car-select-active')
+        optionsChildren = select.querySelectorAll('.car-option-box')
+    }else{
+        offerSelect.classList.add('car-select-active')
+        optionsChildren = offerSelect.querySelectorAll('.car-option-box')
+    }
+    optionsChildren.forEach(item => {
+        item.classList.add('car-option-active')
+    }) 
+
+    if(flag > 0){
+        const parentEl = e.target.closest('.car-option-box');
+        const value = parentEl.dataset.name
+        ctaHidden.value = value
+        e.target.closest('.car-select-btn').classList.remove('car-select-active')
+        optionsChildren.forEach(item => {
+            item.classList.remove('car-option-active')
+            item.classList.remove('first-option')
+        })
+        parentEl.classList.add('first-option')
+        flag = 0
+    }else{
+        flag++
+    }
+}
+
+const currentYear = new Date().getFullYear();
+const yearEl = document.querySelector(".currentyear");
+
+yearEl.textContent = currentYear;
+
+
+const header = document.querySelector('.header')
+const allLinks = document.querySelectorAll("a:link");
+
+allLinks.forEach(function (link) {
+  link.addEventListener("click", function (e) {
+    e.preventDefault();
+    const href = link.getAttribute("href");
+    console.log(href);
+
+    if (href === "#")
+      window.scrollTo({
+        top: 0,
+        behavior: "smooth",
+      });
+    // scrool to other links
+    if (href !== "#" && href.startsWith("#")) {
+      const sectionEl = document.querySelector(href);
+      sectionEl.scrollIntoView({ behavior: "smooth" });
+    }
+
+    // close mobile nav
+    if (header.classList.contains("nav-open")) {
+      header.classList.remove("nav-open");
+    }
+  });
+});
+
+const offerBtn = document.querySelector('.ask-offer')
+const sectionOffer = document.querySelector('.section-offer')
+const clsBtn = document.querySelectorAll('.cls-offer')
+
+clsBtn.forEach(item => {
+    item.addEventListener('click', toggleOffer )
+})
+
+
+offerBtn.addEventListener('click', toggleOffer)
+function toggleOffer(){
+    sectionOffer.classList.toggle('offer-active')
+}
 
